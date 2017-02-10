2e674d4e33b9d882c8718697abd338698205010c
owlsogul <owlsogul@naver.com>
2017-02-10 23:05:12 +0900

    MemoCell Add

---
+++ Ah?IHaveToWatchThis/Ah?IHaveToWatchThis/MemoCell.swift
@@ -0,0 +1,22 @@
+//
+//  MemoCell.swift
+//  Ah?IHaveToWatchThis
+//
+//  Created by CAUADC on 2017. 2. 10..
+//  Copyright © 2017년 owlsogul. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    @IBOutlet weak var watchButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    
+
+}
